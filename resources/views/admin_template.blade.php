5b20411bae64356b70aebdecf98af3972ac6e09b
mr-robot@mr-robot <mr-robot@mr-robot>
2017-02-21 15:29:05 +0700

    fix bug sidebar-toggle

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -63,10 +63,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 </div><!-- ./wrapper -->
 <script src="{{ asset ('/bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+<script src="{{ asset ('/bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
 <!-- Bootstrap 3.3.2 JS -->
 <script src="{{ asset ('/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset ('/bower_components/AdminLTE/dist/js/app.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset ('/bower_components/AdminLTE/dist/js/app.js') }}" type="text/javascript"></script>
+
 @include('table.scripts')
 
 <!-- REQUIRED JS SCRIPTS -->
