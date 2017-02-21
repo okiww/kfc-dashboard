7ebe63554f3c83bb7f0b67eeb8288487919e6640
mr-robot@mr-robot <mr-robot@mr-robot>
2017-02-21 15:10:48 +0700

    fix bug page title

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -6,7 +6,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>{{ $page_title[0] or "FFI DASHBOARD" }}</title>
+    <title>{{ $page_title[0] or "KFC" }}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     @include('table.styles')
 
