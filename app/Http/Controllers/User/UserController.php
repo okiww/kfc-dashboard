14b283c2d659de56d766788cbd79f4f0f23cf5d1
mr-robot@mr-robot <mr-robot@mr-robot>
2017-02-23 12:05:18 +0700

    add UserController

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\User;
+use App\Http\Controllers\Controller;
+use Datatables;
+use DB;
+use Validator;
+use Auth;
+
+class UserController extends Controller
+{
+    public $controller = 'user';
+    public $view = 'users';
+
+    public function __construct()
+    {
+        $this->model = new User;
+        $this->middleware('auth');
+    }
+
+    public function index() {
+        return view('users.index');
+    }
+
+    public function create() {
+    	return view('users.create');
+    }
+
+    public function show($type = null) {
+        if ($type == 'datatables') {
+            $user = Auth::user();
+            $tmp = $this->model->getUsers()->where('id', '!=', $user->id)->get();
+            $data = get_ordinal_numbers($tmp);  
+
+            return Datatables::of($data)
+            ->addColumn('action', function ($data) {
+                $action = null;    
+                $action .= '<a class="btn btn-success btn-xs bs-tooltip btn-show-detail" title="delete">Show
+                    </a>&nbsp;';
+                $action .= '<a class="btn btn-danger btn-xs bs-tooltip btn-confirm-delete" title="delete"'
+                        . ' data-action="'.route('admin::'.$this->controller.'.destroy',$data->id).'"'
+                        . ' data-href="'.route('admin::'.$this->controller.'.show', 'delete').'"'
+                        . ' data-id="'.$data->id.'">Delete</a>&nbsp;';
+
+                 return $action;
+            })->make(true);
+        } elseif ($type == 'detail' || $type == 'delete') {
+            $return = null;
+            $id =request()->input('id');
+            $user = $this->model->getUsers()->where('id', $id)->first();
+                    $return .= '<div class="form-group">';
+                        $return .= '<label class="col-sm-3 control-label"></label>';
+                        $return .= '<div class="col-sm-9">';
+                            $return .= '<p class="form-control-static"></p>';
+                        $return .= '</div>';
+                    $return .= '</div>';
+            return $return;
+        } else {
+            return redirect()->route('admin::'.$this->controller.'.index');
+        }
+    }
+
+    public function store(Request $request) {
+        
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validate->fails()) {
+            return Response::json([
+                'status' => array('code' => 400, 'message' => $validate->errors())
+            ], 400);
+        } else {
+            DB::table('users')->insert([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => bcrypt($request->input('password')),
+            ]);
+
+            flash('Success Creating User');
+            return redirect()->route('admin::'.$this->controller.'.index');
+        }        
+    }
+
+    public function destroy($id)
+    {   
+        try {
+            $user = User::find($id);
+            $user->delete();
+
+        } catch (\Exception $e) {
+            flash($e->getMessage(), 'warning');
+            return redirect()->route('admin::'.$this->controller.'.index');
+        }
+        flash('Success Deleting Data');
+        return redirect()->route('admin::'.$this->controller.'.index');
+    }
+}
