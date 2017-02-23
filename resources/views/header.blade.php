5df2f82b67889fc11471eca8706aa7626af9e3cd
mr-robot@mr-robot <mr-robot@mr-robot>
2017-02-23 18:01:02 +0700

    edit href header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -24,7 +24,7 @@
                     <!-- Menu Toggle Button -->
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <!-- The user image in the navbar-->
-                        <img src="{{ Auth::user()->avatar }}" class="user-image" alt="User Image"/>
+                        <img src="{{ asset(Auth::user()->avatar) }}" class="user-image" alt="User Image"/>
                         <!-- hidden-xs hides the username on small devices so only the image appears. -->
                         <span class="hidden-xs">{{ Auth::user()->name }}</span>
                     </a>
