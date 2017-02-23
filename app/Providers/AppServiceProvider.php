6e4285fb4abf075bb639e484c1d08a1b9ee1bc53
mr-robot@mr-robot <mr-robot@mr-robot>
2017-02-23 12:04:37 +0700

    modifying AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,20 +31,29 @@ class AppServiceProvider extends ServiceProvider
 
     private function bootMenuViewComposer()
     {
-       $this->model = new Menu;
-       $this->model_child = new MenuChild;
-       view()->composer('admin_template', function ($view) {
-           $menu = $this->model->menus();
-           $menus = [];
-           foreach ($menu as $value) {
-               $menus['data'][]= $value->name;
-               $child = $this->model_child->get($value->id);
-               foreach ($child as $key => $val) {
-                   $menus['data'][] =  $val->name;
-               }
-           }
-           \Log::info($menus);
-           $view->withMenus($menus);
-       });
+       // $this->model = new Menu;
+       // $this->model_child = new MenuChild;
+       // view()->composer('admin_template', function ($view) {
+       //     $menu = $this->model->menus();
+       //     $menus = [];
+       //     foreach ($menu as $value) {
+       //         array_push($menus, $value->name);
+       //     }
+
+       //     for ($i=0; $i < count($menus); $i++) { 
+       //        $parent_id = Menu::where('name', $menus[$i])->first();
+       //        $child = MenuChild::select('name')->where('menu_id', $parent_id->id)->get();
+       //        \Log::info($child);
+       //        if (!empty($child)) {
+       //          foreach ($child as $value) {
+       //            $menus[$i] = array($parent_id->name, $value->name); 
+       //          }
+       //        } else {
+       //          $menus[$i] = array($parent_id->name);
+       //        }
+       //     }
+       //     \Log::info($menus);
+       //     $view->withMenus($menus);
+       // });
     }
 }
