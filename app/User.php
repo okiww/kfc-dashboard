efbb5b1ba6d875e32ae572dc6579dee9f148303e
mr-robot@mr-robot <mr-robot@mr-robot>
2017-02-23 12:06:12 +0700

    modify model user add softdeletes

--- app/User.php
+++ app/User.php
@@ -1,16 +1,20 @@
 <?php
 
 namespace App;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use DB;
 
 class User extends Authenticatable
 {
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    public $table = 'users';
     protected $fillable = [
         'name', 'email', 'password',
     ];
@@ -23,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getUsers() {
+        return User::select('*');
+    }
 }
